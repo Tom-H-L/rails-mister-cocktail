7219cf17eaa61105cc3b58ed7b682615e563b95a
Tom Lautenbacher <tom@lautenbacher.biz>
2021-02-19 14:38:03 +0100

    saving...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+
+Cocktail.create!(
+  name: "Caipirinha"
+  )
+
+Cocktail.create!(
+  name: "Vodka Martini"
+  )
+
+Cocktail.create!(
+  name: "Tequila Sunrise"
+  )
+
+Cocktail.create!(
+  name: "Daiquiri"
+  )
+
+Cocktail.create!(
+  name: "Sex on the beach"
+  )
+
+Cocktail.create!(
+  name: "Manhattan"
+  )
+
+Cocktail.create!(
+  name: "Kamikaze"
+  )
